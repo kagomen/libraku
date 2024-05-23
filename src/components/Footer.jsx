9297b6e619127e1b34960e691f0e892628807b51
kagome <fu.taz.1102@gmail.com>
2024-05-24 00:27:16 +0900

    トップページの追加

---
+++ src/components/Footer.jsx
@@ -0,0 +1,5 @@
+export default function Footer() {
+  return (
+    <div>Developed by kagome 2024</div>
+  )
+}
---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+function Hero() {
+  return (
+    <div>
+      <section>
+        <img src="" alt="" />
+        <h2>図書館予約カードの記入をラクにする</h2>
+        <p>図書館で予約・リクエストカードを記入する際に必要な書籍情報を表示します</p>
+      </section>
+      <form>
+        <p>書名・著者名など</p>
+        <input type="text" />
+        <button>検索</button>
+      </form>
+    </div>
+  )
+}
+
+export default Hero
