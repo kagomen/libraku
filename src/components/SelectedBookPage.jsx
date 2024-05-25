44fd4f0de6e9146d948085e8479869d18ca02170
kagome <fu.taz.1102@gmail.com>
2024-05-25 19:53:24 +0900

    テーブルタグをコンポーネント化

--- src/components/SelectedBookPage.jsx
+++ src/components/SelectedBookPage.jsx
@@ -1,4 +1,26 @@
 const SelectedBookPage = ({ data }) => {
+
+  const Tr = (props) => {
+    return (
+      <td className="flex flex-col">{props.children}</td>
+    )
+  }
+  const Th = (props) => {
+    return (
+      <td className="w-fit mx-4 mt-6 mb-2 px-2 py-1 text-xs font-medium bg-emerald-500 text-white rounded">{props.children}</td>
+    )
+  }
+  const Td = (props) => {
+    return (
+      <td className="mx-4 font-semibold text-base">{props.children}</td>
+    )
+  }
+  // th,
+  // td {
+  //   @apply  ;
+  // }
+
+
   return (
     <div>
       <p className="font-bold text-emerald-600 text-center mb-4">書籍情報</p>
@@ -6,34 +28,34 @@ const SelectedBookPage = ({ data }) => {
         <img src={data.volumeInfo?.imageLinks?.thumbnail} className="block mx-auto mt-3" />
         <table className="mx-auto mt-4 text-left w-full">
           <tbody>
-            <tr>
-              <th>書名</th>
-              <td>{data.volumeInfo?.title ?? '-'}</td>
-            </tr>
-            <tr>
-              <th>著者名</th>
-              <td>{data.volumeInfo?.authors.join(' / ') ?? '-'}</td>
-            </tr>
-            <tr>
-              <th>出版社</th>
-              <td>{data.volumeInfo?.publisher ?? '-'}</td>
-            </tr>
-            <tr>
-              <th>出版日</th>
-              <td>{new Date(data.volumeInfo?.publishedDate).toLocaleDateString('ja-JP') ?? '-'}</td>
-            </tr>
-            <tr>
-              <th>価格</th>
-              <td>{data.saleInfo?.listPrice?.amount ?? '-'}円</td>
-            </tr>
-            <tr>
-              <th>利用者番号</th>
-              <td>123-456-7890</td>
-            </tr>
-            <tr>
-              <th>本日の日付</th>
-              <td>{new Date().toLocaleDateString('ja-JP')}</td>
-            </tr>
+            <Tr>
+              <Th>書名</Th>
+              <Td>{data.volumeInfo?.title ?? '-'}</Td>
+            </Tr>
+            <Tr>
+              <Th>著者名</Th>
+              <Td>{data.volumeInfo?.authors.join(' / ') ?? '-'}</Td>
+            </Tr>
+            <Tr>
+              <Th>出版社</Th>
+              <Td>{data.volumeInfo?.publisher ?? '-'}</Td>
+            </Tr>
+            <Tr>
+              <Th>出版日</Th>
+              <Td>{new Date(data.volumeInfo?.publishedDate).toLocaleDateString('ja-JP') ?? '-'}</Td>
+            </Tr>
+            <Tr>
+              <Th>価格</Th>
+              <Td>{data.saleInfo?.listPrice?.amount ?? '-'}円</Td>
+            </Tr>
+            <Tr>
+              <Th>利用者番号</Th>
+              <Td>123-456-7890</Td>
+            </Tr>
+            <Tr>
+              <Th>本日の日付</Th>
+              <Td>{new Date().toLocaleDateString('ja-JP')}</Td>
+            </Tr>
           </tbody>
         </table>
       </div>
