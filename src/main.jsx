ecba42d6c7a4d7777333b14900c3c6241d390929
kagome <fu.taz.1102@gmail.com>
2024-06-19 15:55:18 +0900

    キャッシュ用Contextの削除 #42

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './main.css'
 import { BrowserRouter } from 'react-router-dom'
-import { SearchResultCacheProvider } from './context/SearchResultCache.jsx'
-import { BookDataCacheProvider } from './context/BookDataCache.jsx'
 import { SearchDataProvider } from './context/SearchData.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
@@ -12,16 +10,12 @@ const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SearchResultCacheProvider>
-      <BookDataCacheProvider>
-        <QueryClientProvider client={queryClient}>
-          <BrowserRouter>
-            <SearchDataProvider>
-              <App />
-            </SearchDataProvider>
-          </BrowserRouter>
-        </QueryClientProvider>
-      </BookDataCacheProvider>
-    </SearchResultCacheProvider>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <SearchDataProvider>
+          <App />
+        </SearchDataProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
