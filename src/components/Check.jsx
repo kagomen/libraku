a638b050e903abc01d11fbe87adfea1877fc93e9
kagome <fu.taz.1102@gmail.com>
2024-06-28 15:35:38 +0900

    アイコンの修正 #51

--- src/components/Check.jsx
+++ src/components/Check.jsx
@@ -1,9 +1,9 @@
-import { HiOutlineCheckCircle } from "react-icons/hi";
+import { CircleCheckBig } from "lucide-react";
 
 export default function Check() {
   return (
     <div className="flex mx-auto my-14 w-fit text-emerald-500">
-      <span className="text-2xl pr-2 -translate-y-[3px]"><HiOutlineCheckCircle /></span>
+      <CircleCheckBig size="20" className="mr-2 -translate-y-[0.5px]" />
       <p className="text-sm font-medium">すべてのアイテムを表示しました</p>
     </div>
   )
