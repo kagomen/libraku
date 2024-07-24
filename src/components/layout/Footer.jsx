afbb169c9835a2cd9b732f0313a23437b980b541
kagome <fu.taz.1102@gmail.com>
2024-07-24 20:54:14 +0900

    フッターの修正

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,12 +1,17 @@
 import { Link } from 'react-router-dom'
+import { Button } from '@/components/ui/button'
 
 export default function Footer() {
   return (
-    <div className="sticky top-full flex h-[96px] w-full flex-col items-center justify-center gap-y-2 bg-stone-300">
-      <div className="flex justify-center gap-2 text-sm text-white">
-        <Link to={'/contact'}>お問い合わせ</Link>
+    <div className="sticky top-full flex h-[140px] w-full flex-col items-center justify-center gap-y-5 bg-stone-400/70">
+      <div className="flex items-center justify-center text-sm text-white">
+        <Button variant="link" className="text-white">
+          <Link to={'/contact'}>お問い合わせ</Link>
+        </Button>
         <span>|</span>
-        <Link to={'/about'}>このサイトについて</Link>
+        <Button variant="link" className="text-white">
+          <Link to={'/about'}>このサイトについて</Link>
+        </Button>
       </div>
       <p className="text-center text-xs text-foreground/40">Copyright © 2024 リブラク All Rights Reserved</p>
     </div>
