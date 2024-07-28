562b9f30787beef124824bc5cb19bbedc860ed11
kagome <fu.taz.1102@gmail.com>
2024-07-28 13:53:13 +0900

    drizzle ok

--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -1,13 +1,31 @@
 import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
 import { signUpSchema } from '../lib/schema'
+import { drizzle } from 'drizzle-orm/d1'
+import { users } from '../db/schema'
 
 const router = new Hono()
 
+// ユーザー情報の一覧取得
+router.get('/users', async (c) => {
+	const db = drizzle(c.env.DB)
+	const result = await db.select().from(users).all()
+	return c.json(result)
+})
+
+// ユーザー新規登録
 router.post('/signup', zValidator('json', signUpSchema), async (c) => {
 	const { email, password } = c.req.valid('json')
-	console.log('email', email)
-	console.log('password', password)
+	const db = drizzle(c.env.DB)
+	const result = await db.insert(users).values({
+		email,
+		password,
+	})
+	return c.json(result)
 })
+
+// ログイン
+
+// ログアウト
 
 export default router
