9651dcb4087f1de94e79c79c3252d55d321fa695
kagome <fu.taz.1102@gmail.com>
2024-08-10 15:51:24 +0900

    PWAの実装 #65

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,53 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import { VitePWA } from 'vite-plugin-pwa'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'apple-touch-icon.png'],
+      injectRegister: 'auto',
+      manifest: {
+        name: 'リブラク',
+        short_name: 'リブラク', // ホーム画面に追加した時に表示される名前
+        description: '図書館ユーザーのための書籍検索アプリ',
+        theme_color: '#ffffff',
+        // 各サイズごとのアイコンは下記で生成する。
+        // https://www.pwabuilder.com/imageGenerator
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     proxy: {
       '/api': {
