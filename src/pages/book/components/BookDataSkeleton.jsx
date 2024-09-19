b944049e357897dd76c202c3f9940fd3f5d06110
kagome <fu.taz.1102@gmail.com>
2024-09-19 18:10:24 +0900

    Suspenseまわりの修正 #105

--- src/pages/book/components/BookDataSkeleton.jsx
+++ src/pages/book/components/BookDataSkeleton.jsx
@@ -7,10 +7,6 @@ const BookDataSkeleton = () => {
   return (
     <Card className="space-y-11 py-11">
       <Skeleton className="mx-auto h-[200px] w-[140px]" />
-      <div className="flex items-center justify-center gap-2">
-        <Skeleton className="h-9 w-full" />
-        <Skeleton className="h-9 w-full" />
-      </div>
       <div className="mx-auto mt-10 w-full space-y-6 text-left">
         <div>
           <Title>書名</Title>
@@ -58,56 +54,6 @@ const BookDataSkeleton = () => {
         </div>
       </div>
     </Card>
-
-    // <Card>
-    //   <Skeleton className="mx-auto my-4 block h-[200px] w-[140px]" />
-    //   <div className="mx-auto mt-10 w-full text-left">
-    //     <CardContent>
-    //       <CardTitle>書名</CardTitle>
-    //       <CardDescription>
-    //         <Skeleton className="h-6 w-full" />
-    //       </CardDescription>
-    //     </CardContent>
-    //     <CardContent>
-    //       <CardTitle>著者名</CardTitle>
-    //       <CardDescription>
-    //         <Skeleton className="h-6 w-full" />
-    //       </CardDescription>
-    //     </CardContent>
-    //     <CardContent>
-    //       <CardTitle>出版社</CardTitle>
-    //       <CardDescription>
-    //         <Skeleton className="h-6 w-full" />
-    //       </CardDescription>
-    //     </CardContent>
-    //     <CardContent>
-    //       <CardTitle>出版日</CardTitle>
-    //       <CardDescription>
-    //         <Skeleton className="h-6 w-full" />
-    //       </CardDescription>
-    //     </CardContent>
-    //     <CardContent>
-    //       <CardTitle>価格</CardTitle>
-    //       <CardDescription>
-    //         <Skeleton className="h-6 w-full" />
-    //       </CardDescription>
-    //     </CardContent>
-    //     <CardContent>
-    //       <CardTitle>ISBN</CardTitle>
-    //       <CardDescription>
-    //         <Skeleton className="h-6 w-full" />
-    //       </CardDescription>
-    //     </CardContent>
-    //     <CardContent>
-    //       <CardTitle>利用者番号</CardTitle>
-    //       <CardDescription>123-456-7890</CardDescription>
-    //     </CardContent>
-    //     <CardContent>
-    //       <CardTitle>本日の日付</CardTitle>
-    //       <CardDescription className="border-b-0">{new Date().toLocaleDateString('ja-JP')}</CardDescription>
-    //     </CardContent>
-    //   </div>
-    // </Card>
   )
 }
 
--- src/pages/favorites/components/FavoritesListSkelton.jsx
+++ src/pages/favorites/components/FavoritesListSkelton.jsx
@@ -1,22 +1,31 @@
 import { Card } from '@/components/shadcn-ui/card'
 import { Skeleton } from '@/components/shadcn-ui/skeleton'
+import { Trash2 } from 'lucide-react'
 
 function FavoritesListSkelton() {
   function FavoriteSkelton() {
     return (
-      <Card className="mt-4 flex gap-4 p-4">
-        <Skeleton className="h-[121px] w-[93.5px] shrink-0" />
-        <div className="w-full">
-          <Skeleton className="mb-1 h-6 w-full" />
-          <Skeleton className="h-5 w-[80%]" />
+      <Card className="relative mt-4 p-4">
+        <div className="flex gap-4">
+          <Skeleton className="h-[121px] w-[93.5px] shrink-0 self-start" />
+          <div className="w-full">
+            <Skeleton className="mb-1 h-6 w-full" />
+            <Skeleton className="h-5 w-[80%]" />
+          </div>
         </div>
       </Card>
     )
   }
 
   return (
-    <div className="py-12">
-      <p className="text-sm font-semibold">全 ? 件</p>
+    <div className="pt-12">
+      <div className="flex items-center justify-between">
+        <p className="ml-1.5 text-sm font-semibold">全 ? 件</p>
+        <Skeleton className="flex h-9 w-fit items-center justify-between rounded-md bg-primary px-2 text-xs text-white">
+          <Trash2 size="16" className="mr-2" />
+          全件削除
+        </Skeleton>
+      </div>
       <FavoriteSkelton />
       <FavoriteSkelton />
       <FavoriteSkelton />
